ddc93716fa6a5874c30cd40fdf1b8009059e0cd0
Ali Shouman <alishouman010@gmail.com>
2022-09-13 01:13:07 +0300

    days are left before Christmas

---
+++ api_christmas..php
@@ -0,0 +1,21 @@
+<?php
+//get how many sec to christmas   from  since January 1 1970 00:00:00 GMT.
+$christmas = date('Y-12-25');
+$date = strtotime("now");
+//get how many sec to christmas   from  now.
+$secTochristmas = strtotime($christmas) - $date;
+$secInDay = 86400;
+$secInHour = 1440;
+$secInMin = 60;
+//get it in more  detials
+$days = (int)($secTochristmas / $secInDay);
+$hour = (int)(($secTochristmas % $secInDay) / $secInHour);
+$min = (int)((($secTochristmas % $secInDay) % $secInHour) / $secInMin);
+$sec = (int)(((($secTochristmas % $secInDay) % $secInHour) % $secInMin));
+$results = [
+  "days" => $days,
+  "hour" => $hour,
+  "min" => $min,
+  "sec" => $sec
+];
+echo json_encode($results);
